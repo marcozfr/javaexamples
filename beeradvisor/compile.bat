227a8bf19350df1fe48a5100640b732bf9ec4030
marcozfr <marcozfr@gmail.com>
2016-08-17 16:32:53 -0500

    compile details
    compile details

--- beeradvisor/compile.bat
+++ beeradvisor/compile.bat
@@ -1,3 +1,3 @@
-javac -cp ..\..\lib\servlet-api.jar -d web\beeradvisor\WEB-INF\classes src\com\example\web\BeerServlet.java
-mkdir ..\..\webapps\beeradvisor
-xcopy web\beeradvisor ..\..\webapps\beeradvisor /s /e /y
+javac -cp C:\opt\apache-tomcat-8.0.33\lib\servlet-api.jar -d web\beeradvisor\WEB-INF\classes src\com\example\web\BeerServlet.java
+mkdir C:\opt\apache-tomcat-8.0.33\webapps\beeradvisor
+xcopy web\beeradvisor C:\opt\apache-tomcat-8.0.33\webapps\beeradvisor /s /e /y
