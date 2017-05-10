82e5fbce2e55bd785a987c0d9d0182be1eb59e92
marco <marcozfr@gmail.com>
2017-05-10 17:54:44 -0500

    ah

--- jaxws-local/src/main/java/com/example/ws/process/server/dto/File.java
+++ jaxws-local/src/main/java/com/example/ws/process/server/dto/File.java
@@ -1,8 +1,15 @@
 package com.example.ws.process.server.dto;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlMimeType;
+
+@XmlAccessorType(XmlAccessType.FIELD)
 public class File {
 	
 	private String name;
+	
+	@XmlMimeType("image/jpeg")
 	private byte[] content;
 	
 	public String getName() {
